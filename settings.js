346b80e0852783738a413896bc0399c012c059a4
leviscop <git@leviscop.dev>
2021-10-12 15:30:41 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,65 +1,65 @@
 module.exports = {
 	info: {
-		name: process.env.COMPANY_NAME,
-		url: process.env.SUPPORT_URL
+		name: process.env.AD_COMPANY_NAME,
+		url: process.env.AD_SUPPORT_URL
 	},
-	domain: process.env.DOMAIN,
+	domain: process.env.AD_DOMAIN,
 	imap: {
-		host: process.env.IMAP_HOST,
-		port: process.env.IMAP_PORT,
-		socket: process.env.IMAP_SOCKET
+		host: process.env.AD_IMAP_HOST,
+		port: process.env.AD_IMAP_PORT,
+		socket: process.env.AD_IMAP_SOCKET
 	},
 	imaps: {
-		host: process.env.IMAPS_HOST,
-		port: process.env.IMAPS_PORT,
-		socket: process.env.IMAPS_SOCKET
+		host: process.env.AD_IMAPS_HOST,
+		port: process.env.AD_IMAPS_PORT,
+		socket: process.env.AD_IMAPS_SOCKET
 	},
 	pop: {
-		host: process.env.POP_HOST,
-		port: process.env.POP_PORT,
-		socket: process.env.POP_SOCKET
+		host: process.env.AD_POP_HOST,
+		port: process.env.AD_POP_PORT,
+		socket: process.env.AD_POP_SOCKET
 	},
 	pops: {
-		host: process.env.POPS_HOST,
-		port: process.env.POPS_PORT,
-		socket: process.env.POPS_SOCKET
+		host: process.env.AD_POPS_HOST,
+		port: process.env.AD_POPS_PORT,
+		socket: process.env.AD_POPS_SOCKET
 	},
 	smtp: {
-		host: process.env.SMTP_HOST,
-		port: process.env.SMTP_PORT,
-		socket: process.env.SMTP_SOCKET
+		host: process.env.AD_SMTP_HOST,
+		port: process.env.AD_SMTP_PORT,
+		socket: process.env.AD_SMTP_SOCKET
 	},
 	smtps: {
-		host: process.env.SMTPS_HOST,
-		port: process.env.SMTPS_PORT,
-		socket: process.env.SMTPS_SOCKET
+		host: process.env.AD_SMTPS_HOST,
+		port: process.env.AD_SMTPS_PORT,
+		socket: process.env.AD_SMTPS_SOCKET
 	},
 	submission: {
-		host: process.env.SUBMISSION_HOST,
-		port: process.env.SUBMISSION_PORT,
-		socket: process.env.SUBMISSION_SOCKET
+		host: process.env.AD_SUBMISSION_HOST,
+		port: process.env.AD_SUBMISSION_PORT,
+		socket: process.env.AD_SUBMISSION_SOCKET
 	},
 	mobilesync: {
-		url: process.env.MOBILESYNC_URL,
-		name: process.env.MOBILESYNC_NAME
+		url: process.env.AD_MOBILESYNC_URL,
+		name: process.env.AD_MOBILESYNC_NAME
 	},
 	ldap: {
-		host: process.env.LDAP_HOST,
-		port: process.env.LDAP_PORT,
-		socket: process.env.LDAP_SOCKET,
-		base: process.env.LDAP_BASE,
-		userfield: process.env.LDAP_USER_FIELD,
-		usersbase: process.env.LDAP_USER_BASE,
-		searchfilter: process.env.LDAP_SEARCH
+		host: process.env.AD_LDAP_HOST,
+		port: process.env.AD_LDAP_PORT,
+		socket: process.env.AD_LDAP_SOCKET,
+		base: process.env.AD_LDAP_BASE,
+		userfield: process.env.AD_LDAP_USER_FIELD,
+		usersbase: process.env.AD_LDAP_USER_BASE,
+		searchfilter: process.env.AD_LDAP_SEARCH
 	},
 	mobile: {
-		identifier: process.env.PROFILE_IDENTIFIER,
-		uuid: process.env.PROFILE_UUID,
+		identifier: process.env.AD_PROFILE_IDENTIFIER,
+		uuid: process.env.AD_PROFILE_UUID,
 		mail: {
-			uuid: process.env.MAIL_UUID,
+			uuid: process.env.AD_MAIL_UUID,
 		},
 		ldap: {
-			uuid: process.env.LDAP_UUID,
+			uuid: process.env.AD_LDAP_UUID,
 		}
 	}
 };
